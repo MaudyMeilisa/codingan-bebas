779a1edbec10810a6735643f3067215d3b5eb812
MaudyMeilisa <maudymeilisa3@gmail.com>
2021-09-19 09:33:44 +0700

    latihan

--- kalkulator.php
+++ kalkulator.php
@@ -4,83 +4,133 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kalkulator</title>
+    <title>Document</title>
 </head>
 <body>
-    <form method="POST">
-    <input type="text" name="x"> &nbsp;
-   
-    <select name="operasi">
-      <option value="tambah">+</option>
-      <option value="kali">x</option>
-      <option value="kurang">-</option>
-      <option value="bagi">/</option>
-    </select>
-   
-    <input type="text" name="y"> &nbsp;
-    <label>=</label> 
-    <input type="submit" value="Hitung">
-</form>
+<table>
+        <form action="" method="post">
+            <tr>
+                
+                <td><input type="text" name="angka1" id=""></td>
+            <td>
+                <select class="opt" name="operasi">
+				<option value="+">+</option>
+				<option value="-">-</option>
+				<option value="X">x</option>
+				<option value="/">/</option>
+			</select></td>
+                <td><input type="text" name="angka2" id=""></td>
+           
+                
+                <td><input type="submit" name= "hitung" value="Hitung"></td>
+            </tr>
+        </form>
+    </table>
 </body>
 </html>
 
-
 <?php
-$data = new kalkulator();
-if (isset($_POST['y'])){
-  $x = $_POST['x'];
-  $y = $_POST['y'];
-  if ($_POST['operasi'] == "tambah") {
-    echo "Hasil ".$x." + ".$y." = ".$data->Tambah($x, $y).
-    "<br /><br />";
-  } elseif ($_POST['operasi'] == "kali") {
-    echo "Hasil ".$x." x ".$y." = ".$data->Kali($x, $y).
-    "<br /><br />";
-  } elseif ($_POST['operasi'] == "kurang") {
-    echo "Hasil ".$x." - ".$y." = ".$data->Kurang($x, $y).
-    "<br /><br />";
-  } elseif ($_POST['operasi'] == "bagi") {
-    echo "Hasil ".$x." / ".$y." = ".$data->Bagi($x, $y).
-    "<br /><br />";
-  }
-}
- 
-class kalkulator
-{
-  public $x;
-  public $y;
- 
-  public function Tambah($x, $y)
-  {
-    $this->x = $x;
-    $this->y = $y;
-    $hasil = $this->x + $this->y;
-    return $hasil;
-  }
- 
-  public function Kali($x, $y)
-  {
-    $this->x = $x;
-    $this->y = $y;
-    $hasil = $this->x * $this->y;
-    return $hasil;
-  }
- 
-  public function Kurang($x, $y)
-  {
-    $this->x = $x;
-    $this->y = $y;
-    $hasil = $this->x - $this->y;
-    return $hasil;
-  }
- 
-  public function Bagi($x, $y)
-  {
-    $this->x = $x;
-    $this->y = $y;
-    $hasil = $this->x / $this->y;
-    return $hasil;
-  }
+
+if (isset($_POST["hitung"])) {
+   $angka1 = $_POST['angka1'];
+   $angka2 = $_POST['angka2'];
+    $oprasi = $_POST['operasi'];
+    
+   class Matematika
+   {
+       public $x;
+       public $y;
+       public $z;
+      
+      
+
+       public function tambah($x,$y,$z)
+       {
+           $this->x = $x;
+           $this->y = $y;
+           $this->z = $z;
+           
+
+           if ($z == "+") {
+               
+            $hasil = $x + $y;
+            echo "Hasil ";
+            echo $x;
+            echo " ";
+            echo $z;
+            echo " ";
+            echo $y;
+            echo " = ";
+            echo $hasil;
+           }
+    
+       }
+       public function kurang($x,$y,$z){
+        $this->x = $x;
+        $this->y = $y;
+        $this->z = $z;
+
+        if ($z == "-") {
+               
+            $hasil = $x - $y;
+            echo "Hasil ";
+            echo $x;
+            echo " ";
+            echo $z;
+            echo " ";
+            echo $y;
+            echo " = ";
+            echo $hasil;
+           }
+       }
+       public function kali($x,$y,$z){
+        $this->x = $x;
+        $this->y = $y;
+        $this->z = $z;
+
+        if ($z == "X") {
+               
+            $hasil = $x * $y;
+            echo "Hasil ";
+            echo $x;
+            echo " ";
+            echo $z;
+            echo " ";
+            echo $y;
+            echo " = ";
+            echo $hasil;
+           }
+           
+       }
+       public function bagi($x,$y,$z){
+        $this->x = $x;
+        $this->y = $y;
+        $this->z = $z;
+        
+        if ($z == "/") {
+               
+            $hasil = $x / $y;
+            echo "Hasil ";
+            echo $x;
+            echo " ";
+            echo $z;
+            echo " ";
+            echo $y;
+            echo " = ";
+            echo $hasil;
+           }
+       }
+   }
+
+   $data = new Matematika();
+   $data->tambah($angka1,$angka2,$oprasi);
+   $data->kurang($angka1,$angka2,$oprasi);
+   $data->kali($angka1,$angka2,$oprasi);
+   $data->bagi($angka1,$angka2,$oprasi);
+
+   
+   
+   
 }
 
-
+?>
